d8ecc16ce437fe53945dd726a65f2b44059a572f
SphinxAisso <amine.aisso@outlook.fr>
2018-08-30 13:05:57 +0200

    Setting up sequelize ovm

--- app.js
+++ app.js
@@ -5,9 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var sqlite3 = require('sqlite3').verbose();
-var app = express();
 const { sequelize } = require('./models')
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,65 +19,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-/* ----------------------------------- SQLITE3------------------------------------*/
-var db = new sqlite3.Database('data/db.sql', sqlite3.OPEN_READWRITE, (err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Connected to the SQL database.');
-});
-
-db.serialize(function () {
-    db.run("CREATE TABLE IF NOT EXISTS entreprises (key INTEGER, name TEXT)");
-    db.run("INSERT INTO entreprises (key, name) VALUES (?, ?)", "1", "Zsoft");
-});
-
-let entreprises = `SELECT DISTINCT Name name FROM entreprises
-           ORDER BY name`;
-
-db.all(entreprises, [], (err, rows) => {
-    if (err) {
-        throw err;
-    }
-    rows.forEach((row) => {
-        console.log(row.name);
-    });
-});
-/*
-db.close((err) => {
-    if (err) {
-        console.error(err.message);
-    }
-    console.log('Close the database connection.');
-});
-*/
-app.get('/api/data', function (req, res) {
-    console.log('Test 1');
-    db.get("SELECT * FROM entreprises", function (err, row) {
-        res.json(row);
-    });
-    console.log('Test 2');
-});
-
-app.post('/api/data', function (req, res) {
-    console.log("received Data: " + req.body.json);
-    db.run("INSERT INTO entreprises (key, name) VALUES (?,?)", function(err, row){
-        if (err){
-            console.err(err);
-            res.status(500);
-        }
-        else {
-            console.log(row)
-            res.status(202);
-        }
-        res.end();
-    });
-});
-
 /*---------------------------------------Sequelize--------------------------------*/
 sequelize.sync()
     .then(() => {
-        //app.listen(3000)
+        app.listen(3000);
         console.log('Serveur started on port 3000');
     });
 
