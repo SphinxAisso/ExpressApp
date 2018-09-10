7ca454dd5a3095d015c32db2e66a1ca632b91a92
SphinxAisso <amine.aisso@outlook.fr>
2018-09-10 17:47:17 +0200

    ES6

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    console.log(req.session.passport.user);
+router.get('/', (req, res, next) => {
+    console.log(req.session.passport);
     console.log(req.isAuthenticated());
     res.render('index', { title: 'Express' });
 });
